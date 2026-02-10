34fe2d72d5bb08f2c3cebec82b7fa547cd519ac6
gidi <gidif2004@gmail.com>
2026-02-10 12:04:01 +0200

    add lable_packet_size function

--- checks.py
+++ checks.py
@@ -7,5 +7,9 @@ def sensitive_ports_filter(data):
     return list(filter(lambda log: log[3] in SENSITIVE_PORT,data))
 
 def big_packets_filter(data):
-    return list(filter(lambda log: int(log[-1]) >= 5000, data))
+    return list(filter(lambda log: int(log[5]) >= LARGE_PACKET, data))
 
+def lable_packet_size(data):
+    return list(map(lambda log: log + ['LARGE'] if int(log[5]) >= LARGE_PACKET else log + ['NORMAL'] , data))
+
+print (lable_packet_size([[1,2,5,2,2,'5436'],[1,2,2,2,2,'4436'],[1,2,2,2,2,'6436'],[1,2,2,2,2,'436']]))
