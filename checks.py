fc0f5b8b758ddacb3dd33ca7f0fa343a420a82b8
gidi <gidif2004@gmail.com>
2026-02-10 11:40:42 +0200

    add sensitive_ports_filter function

--- checks.py
+++ checks.py
@@ -1,9 +1,8 @@
 from config import *
 
 def external_ips_filter(data):
-    external_ips = []
-    for log in data:
-        if not log[1].startswith(LOCAL_IPs):
-            external_ips.append(log[1])
-    return external_ips
+    return [log[1] for log in data if not log[1].startswith(LOCAL_IPs)]
+    
+def sensitive_ports_filter(data):
+    return list(filter(lambda log: log[3] in SENSITIVE_PORT,data))
 
